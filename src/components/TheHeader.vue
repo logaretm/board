3e49901e32e4f1563f42e7aeaac49c37ab97f771
Abdelrahman Awad <logaretm1@gmail.com>
2018-12-09 19:50:41 +0200

    feat: added github link to the header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,17 +3,22 @@
   .TheHeader-logo
     RouterLink(to="/"): AppIcon(name="logo")
 
-  .TheHeader-actions
-    AppDropdown.is-large.is-right
-      AppIcon(name="notifications" slot="trigger" type="outlined")
-      AppList(:items="notificationIcons")
-      button.button.is-tiny.is-dark.is-wide.u-mt-small View all
-    AppDropdown.is-right
-      Avatar.is-tiny(slot="trigger" src="/img/users/1.jpg" alt="AV")
-      a.dropdown-item(href='#0') Profile
-      a.dropdown-item(href='#0') Messages
-      a.dropdown-item(href='#0') Logout
-    .TheHeader-icon.is-sidebar(@click.prevent="$emit('sidebar')")
+  ul.TheHeader-actions
+    li.TheHeader-icon
+      a(href="https://github.com/baianat/board" target="blank"): AppIcon(name="github")
+    li
+      AppDropdown.is-large.is-right
+        AppIcon(name="notifications" slot="trigger" type="outlined")
+        AppList(:items="notificationIcons")
+        button.button.is-tiny.is-dark.is-wide.u-mt-small View all
+    li
+      AppDropdown.is-right
+        Avatar(src="/img/users/1.jpg" slot="trigger")
+        a.dropdown-item(href='#0') Profile
+        a.dropdown-item(href='#0') Messages
+        a.dropdown-item(href='#0') Logout
+
+    li.TheHeader-icon.is-sidebar(@click.prevent="$emit('sidebar')")
       AppIcon(name="menu")
 
 </template>
@@ -32,58 +37,46 @@
   top: 0
   right: 0
   left: 0
-
   +mq($until: 'desktop')
     padding: 0 30px
-
   +mq($until: 'mobile')
     padding: 0 20px
-
   &-logo
     max-height: 40px
-
     >a
       display: block
-
     img
       max-height: 40px
-
     .icon-logo
       height: 26px
       width: 90px
       fill: $white
-
   &-icon
     font-size: 24px
     cursor: pointer
-
     .icon
       fill: $white
       display: block
-
     &.is-sidebar
       +mq($from: 'desktop')
         display: none
-
   &-actions
     display: flex
     align-items: center
     margin: 0 -10px
-
+    padding: 0
+    list-style-type: none
     .dropdown
       margin: 0 !important
-
     .dropdown-toggle
       height: 60px
       display: flex
       align-items: center
       cursor: pointer
-
       >.icon
         fill: $white
         font-size: 24px
-
-    >div
+    >div, >li
       padding: 0 10px
       margin: 0
 </style>
